7c61963b69f44d80216388077cd5d2ae9e0053dd
Santiago Villavicencio <66896776+santiago200031@users.noreply.github.com>
2020-07-14 21:11:36 -0500

    Update Procesos.java

--- metodologiasAgiles/MetodologiasAgiles/src/ClasesJava/Procesos.java
+++ metodologiasAgiles/MetodologiasAgiles/src/ClasesJava/Procesos.java
@@ -5,8 +5,6 @@ package ClasesJava;
  *
  * @author villa
  */
-//Realizar un método que devuelva la suma de dos numeros para despues llamarlo en el programa principal.
-
 
 public class Procesos {
     
