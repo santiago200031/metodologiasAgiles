978f6a1a620d0b35c8bffeae2b78aae34009ced3
ChrisCuesta <chriscuesta02@hotmail.com>
2020-07-15 13:59:51 -0500

    Ultimo Comentario

--- metodologiasAgiles/MetodologiasAgiles/src/ClasesJava/Procesos.java
+++ metodologiasAgiles/MetodologiasAgiles/src/ClasesJava/Procesos.java
@@ -9,9 +9,7 @@ public class Procesos {
     //Realizar un método que devuelva la suma de dos numeros para despues llamarlo en el programa principal.
     //El metodo debe tener el nombre *sumar*
     //Debe recibir dos parametros de tipo flotante
-    //Hola Primo
-    //Comentario 1
-    
+
     public float sumar(float a, float b) {
         return a + b;
     }
