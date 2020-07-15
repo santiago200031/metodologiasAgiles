295efd9fc16766ef387568f6a57e617f2ec3293a
Santiago Villavicencio <villavicencioandrs@gmail.com>
2020-07-14 21:19:07 -0500

    Comentario

--- metodologiasAgiles/MetodologiasAgiles/src/ClasesJava/Procesos.java
+++ metodologiasAgiles/MetodologiasAgiles/src/ClasesJava/Procesos.java
@@ -6,5 +6,6 @@ package ClasesJava;
  */
 public class Procesos {
     //Realizar un método que devuelva la suma de dos numeros para despues llamarlo en el programa principal.
+    //El metodo debe tener el nombre *sumar*
 
 }
